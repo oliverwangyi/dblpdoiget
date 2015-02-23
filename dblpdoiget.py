c883aef4b672c0e0c49b793e831f9c70a75495c4
OLIVER YI WANG <yiw2@uci.edu>
2015-02-23 11:08:11 -0800

    Initial Commit
    implemented the simplest doi finder

---
+++ dblpdoiget.py
@@ -0,0 +1,64 @@
+__author__ = 'oliveryiwang'
+
+import urllib
+import urllib2
+from bs4 import BeautifulSoup
+import cookielib
+import os
+import sys
+import re
+import csv
+import codecs
+
+reload(sys)
+sys.setdefaultencoding("utf-8")
+
+response = urllib2.urlopen('http://dblp1.uni-trier.de/pers/hd/r/Redmiles:David_F=')
+html_src = response.read()
+
+#print html_src
+
+parser = BeautifulSoup(html_src)
+#soup = parser.findAll("div", attrs = {'class': 'hide-body'})
+
+publications = parser.findAll("ul", attrs = {'class': 'publ-list'})
+
+#find the mappings between DOI and Title for journal paper
+
+journalpub = parser.findAll('li', attrs = {'class': 'entry article'})
+
+journalpaper = []
+
+for item in journalpub:
+    tempdoi = item.find('a', href=True)
+    doi = tempdoi['href']
+    temptitle = item.find('span', attrs = {'class': 'title'})
+    title = temptitle.text
+    journalpaper.append([doi, title])
+
+#find the mappings between DOI and Title for conference paper
+
+confpub = parser.findAll('li', attrs = {'class': 'entry inproceedings'})
+
+confpaper = []
+
+for item in confpub:
+    tempdoi = item.find('a', href=True)
+    doi = tempdoi['href']
+    temptitle = item.find('span', attrs = {'class': 'title'})
+    title = temptitle.text
+    confpaper.append([doi, title])
+
+#output to two csv file
+try:
+    os.stat('./output')
+except:
+    os.mkdir('./output')
+
+with codecs.open('./output/journalpaper.csv', 'w', 'utf-8') as fout:
+    fw = csv.writer(fout, delimiter = ',')
+    fw.writerows(journalpaper)
+
+with codecs.open('./output/confpaper.csv', 'w', 'utf-8') as fout:
+    fw = csv.writer(fout, delimiter = ',')
+    fw.writerows(confpaper)
